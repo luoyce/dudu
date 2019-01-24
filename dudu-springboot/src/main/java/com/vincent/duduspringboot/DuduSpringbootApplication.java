772a1b5a513ec31a37e133fee88950ee7bb83ba7
luoyce <915155754@qq.com>
2019-01-24 22:23:14 +0800

    124

---
+++ dudu-springboot/src/main/java/com/vincent/duduspringboot/DuduSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.vincent.duduspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuduSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuduSpringbootApplication.class, args);
+    }
+
+}
+
