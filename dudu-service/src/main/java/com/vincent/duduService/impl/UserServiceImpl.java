5c9b351f803e79d8edf75117f3adda6216990052
xuejuan.huang <xuejuan.huang@vstecs.com>
2019-02-13 23:52:58 +0800

    maven version

--- dudu-service/src/main/java/com/vincent/duduService/impl/UserServiceImpl.java
+++ dudu-service/src/main/java/com/vincent/duduService/impl/UserServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserRPCService,UserService {
 
-    @Autowired
+    //@Autowired
     private UserDao userDao;
 
     public List<User> getAll(){
